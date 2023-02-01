e95ff8faad0b5a23722ee4fc5dc7d8569bc791ee
dgmaikon <dgmaikon@pop-os.localdomain>
2023-02-01 12:54:40 -0300

    Attempt to add ScrollFunction

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-
+import MyPage from "./nav";
 
 const Navbar = () => {
     return (
-        <div className="navbar">
-            <nav className="nav">
+        <nav id="navbarId">
+            <div className="nav">
                 <a className="nav-item" href="#typeWriter">Home</a>
                 <a className="nav-item" href="#projects" >Projects</a>
                 <a className="nav-item" href="#about" >About</a>
-            </nav>
-        </div>
+            </div>
+        </nav>
     );
 }
 
