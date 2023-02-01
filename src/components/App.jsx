289f90303581228cc35d4f37d482c18d723d6ce5
dgmaikon <dgmaikon@pop-os.localdomain>
2023-02-01 12:53:33 -0300

    Add import to skills and about stylesheet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,31 @@
-import { useState } from 'react';
+import react , { useState } from 'react';
 import ReactDOM from 'react-dom/client'
 import { Typewriter } from 'react-simple-typewriter';
 
+//Components JSX
 import Navbar from './Navbar/Navbar.jsx';
 import MyComponent from "./Header/typeWriter.jsx";
 import About from './About/About.jsx';
+import Skills from './skills/skills.jsx';
 
+//JS
+import '../components/Navbar/nav.js';
+
+//CSS
 import '../components/root.css'
 import '../components/Header/header.css'
 import '../components/Navbar/navbar.css'
 import '../components/About/about.css'
+import '../components/skills/skills.css'
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
       <MyComponent />
       <About />
-    </div>
+      <Skills />
+    </>
   );
 }
 
