ba305bd0222f44b4e10d93f57e7c253f1278835d
dgmaikon <dgmaikon@pop-os.localdomain>
2023-02-01 12:57:53 -0300

    Create a folder to skills

---
+++ src/components/skills/skills.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Skills = () => {
+    return (
+        <section className="skills">
+            <button className="glowing-btn"><span className='glowing-txt'>S<span className='faulty-letter'>K</span>ILLS</span></button>
+            <p className="skills-text">Estudo linguagens como</p>
+        </section>
+    );
+}
+
+export default Skills;
