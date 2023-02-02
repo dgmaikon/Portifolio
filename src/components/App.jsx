636e8041d60ecbbe067ab1bbd9986a3a60627ca2
dgmaikon <dgmaikondev@gmail.com>
2023-02-02 08:27:59 -0300

    Add createEntry to create projects cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,27 +4,46 @@ import { Typewriter } from 'react-simple-typewriter';
 
 //Components JSX
 import Navbar from './Navbar/Navbar.jsx';
-import MyComponent from "./Header/typeWriter.jsx";
+import Header from "./Header/typeWriter.jsx";
 import About from './About/About.jsx';
-import Skills from './skills/skills.jsx';
+import Skills from './Skills/Skills.jsx';
+import Works from './Works/Works.jsx';
 
 //JS
-import '../components/Navbar/nav.js';
+import projects from './Works/works.js';
+import './Navbar/nav.js';
 
 //CSS
-import '../components/root.css'
-import '../components/Header/header.css'
-import '../components/Navbar/navbar.css'
-import '../components/About/about.css'
-import '../components/skills/skills.css'
+import './root.css'
+import './Header/header.css'
+import './Navbar/navbar.css'
+import './About/about.css'
+import './Skills/skills.css'
+import './Works/works.css'
+
+function createEntry(workEntry) {
+  return (
+    <Works 
+      key={workEntry.id}
+      id={workEntry.id}
+      title={workEntry.title}
+      content={workEntry.content}
+    />
+  )
+}
+
 
 function App() {
   return (
     <>
       <Navbar />
-      <MyComponent />
+      <Header />
       <About />
       <Skills />
+      <div id='works'>
+      <button className="glowing-btn"><span className='glowing-txt'>W<span className='faulty-letter'>O</span>RKS</span></button>
+        {projects.map(createEntry)}
+      </div>
     </>
   );
 }
