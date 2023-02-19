c41d50c8f5a7eeeb3d12bbaa18fc67a48c35a0a2
dgmaikon <dgmaikondev@gmail.com>
2023-02-19 18:56:35 -0300

    Add more Objects to work section.

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -2,13 +2,34 @@ const projects = [
     {
         id: 1,
         title: "Blog Ejs",
-        content: "This projects is based on..."
+        content: "This projects is build with HTML, CSS and EJS"
     },
     {
         id: 2,
         title: "Simon Game",
-        content: "This projects is based on..."
+        content: "This projects is build with HTML"
+    },
+    {
+        id: 3,
+        title: "Tin Dog",
+        content: "This Website is about Tinderfor dogs, if you love your dog i recommend use that!"
+    },
+    {
+        id: 4,
+        title: "Drum Kit",
+        content: "This project is for music lovers and people who like a little sound "
+    },
+    {
+        id: 5,
+        title: "To do List",
+        content: "This project is for to-do lists"
+    },
+    {
+        id: 6,
+        title: "Keeper App",
+        content: "This project is a way to keep your notes up to date"
     }
+    
 ]
 
 export default projects;
