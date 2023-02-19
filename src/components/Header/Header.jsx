3767344c8f1f6bf9ebc4ec5bd77b02cb4812ecd4
dgmaikon <dgmaikondev@gmail.com>
2023-02-19 18:36:34 -0300

    Change name TypeWriter to Header and bntToTop.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Typewriter } from "react-simple-typewriter";
+import "./header.css";
+
+const Header = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    scrolled > 50 ? setVisible(true) : setVisible(false);
+  };
+
+  const clickTop = () => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Oper
+  };
+
+  window.addEventListener("scroll", toggleVisible);
+
+  return (
+    <div id="header">
+      <h1 className="main-title">
+        Maikon Natã{" "}
+        <span style={{ color: "aqua", fontWeight: "bold" }}>
+          {/* Style will be inherited from the parent element */}
+          <Typewriter
+            words={[
+              "eu sou",
+              "Gamer",
+              "Entusiasta de tecnologia",
+              "Front-End Developer",
+            ]}
+            loop={2}
+            cursor
+            cursorStyle="_"
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </span>
+      </h1>
+      <button
+        className="myBtn"
+        style={{ display: visible ? "inline" : "none" }}
+        onClick={clickTop}
+      >
+        TOP
+      </button>
+    </div>
+  );
+};
+
+export default Header;
