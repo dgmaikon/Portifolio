04287aa2ac0629e2ff64cc2d20beab8a23e3a5ca
dgmaikon <dgmaikondev@gmail.com>
2023-02-19 18:32:23 -0300

    Remove all imports of css files to respectives components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,31 @@
-import react , { useState } from 'react';
-import ReactDOM from 'react-dom/client'
-import { Typewriter } from 'react-simple-typewriter';
+import react, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { Typewriter } from "react-simple-typewriter";
 
 //Components JSX
-import Navbar from './Navbar/Navbar.jsx';
-import Header from "./Header/typeWriter.jsx";
-import About from './About/About.jsx';
-import Skills from './Skills/Skills.jsx';
-import Works from './Works/Works.jsx';
+import Navbar from "./Navbar/Navbar.jsx";
+import Header from "./Header/Header.jsx";
+import About from "./About/About.jsx";
+import Skills from "./Skills/Skills.jsx";
+import Works from "./Works/Works.jsx";
+import Title from "./Works/Title-Work.jsx";
 
 //JS
-import projects from './Works/works.js';
-import './Navbar/nav.js';
+import projects from "./Works/works.js";
 
 //CSS
-import './root.css'
-import './Header/header.css'
-import './Navbar/navbar.css'
-import './About/about.css'
-import './Skills/skills.css'
-import './Works/works.css'
+import "./App.css";
 
 function createEntry(workEntry) {
   return (
-    <Works 
+    <Works
       key={workEntry.id}
       id={workEntry.id}
       title={workEntry.title}
       content={workEntry.content}
     />
-  )
+  );
 }
-
 
 function App() {
   return (
@@ -40,10 +34,8 @@ function App() {
       <Header />
       <About />
       <Skills />
-      <div id='works'>
-      <button className="glowing-btn"><span className='glowing-txt'>W<span className='faulty-letter'>O</span>RKS</span></button>
-        {projects.map(createEntry)}
-      </div>
+      <Title />
+      <div className="projects">{projects.map(createEntry)}</div>
     </>
   );
 }
