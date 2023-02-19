5d960d4dafd79769f8bb627b38d1279378e7bdaa
dgmaikon <dgmaikondev@gmail.com>
2023-02-19 18:59:21 -0300

    Add file and Title of sections.

---
+++ src/components/Works/Title-Work.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Title = () => {
+    return <button className="glowing-btn"><span className='glowing-txt'>W<span className='faulty-letter'>O</span>RKS</span></button>
+}
+
+export default Title;
