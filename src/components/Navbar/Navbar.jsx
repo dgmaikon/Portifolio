f4c9374d2e5e95ed6c24330627bda74bf63dd388
dgmaikon <dgmaikondev@gmail.com>
2023-02-19 18:42:08 -0300

    Change id selector to class selector.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,25 @@
-import React from "react";
-import MyPage from "./nav";
+import React, { useState, useEffect } from "react";
+import "./navbar.css";
 
 const Navbar = () => {
-    return (
-        <nav id="navbarId">
-            <div className="nav">
-                <a className="nav-item" href="#header">Home</a>
-                <a className="nav-item" href="#about" >About</a>
-                <a className="nav-item" href="#skills" >Skills</a>
-                <a className="nav-item" href="#projects" >Projects</a>
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav>
+      <div className="nav">
+        <a className="nav-item" href="#header">
+          Home
+        </a>
+        <a className="nav-item" href="#about">
+          About
+        </a>
+        <a className="nav-item" href="#skills">
+          Skills
+        </a>
+        <a className="nav-item" href="#projects">
+          Projects
+        </a>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
