7ba6c23591242225cb0a47b096ffeffed3214c2b
dgmaikon <dgmaikondev@gmail.com>
2023-02-19 18:29:08 -0300

    Identity the code and change import directory

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import "./about.css";
 
 const About = () => {
-    return (
-        <div id="about">
-            <button className="glowing-btn"><span className='glowing-txt'>A<span className='faulty-letter'>B</span>OUT</span></button>
-            <p className="about-text">Olá me chamo Maikon Natã sou Front-End Developer, 
-               <span> Estou sempre procurando aprender coisas novas e melhorando a cada dia.</span>
-            </p>
-        </div>
-    );
-}
+  return (
+    <div id="about">
+      <button className="glowing-btn">
+        <span className="glowing-txt">
+          A<span className="faulty-letter">B</span>OUT
+        </span>
+      </button>
+      <p className="about-text">
+        Olá me chamo Maikon Natã sou Front-End Developer,
+        <span>
+          {" "}
+          Estou sempre procurando aprender coisas novas e melhorando a cada dia.
+        </span>
+      </p>
+    </div>
+  );
+};
 
 export default About;
