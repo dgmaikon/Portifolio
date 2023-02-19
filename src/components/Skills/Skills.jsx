053694003f4f234c5506f00d8232034758525608
dgmaikon <dgmaikondev@gmail.com>
2023-02-19 18:53:35 -0300

    Indented the code and change import directoy.

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import "./skills.css";
 
 const Skills = () => {
-    return (
-        <section id="skills">
-            <button className="glowing-btn"><span className='glowing-txt'>S<span className='faulty-letter'>K</span>ILLS</span></button>
-            <p className="skills-text">Estudo linguagens como</p>
-        </section>
-    );
-}
+  return (
+    <section id="skills">
+      <button className="glowing-btn">
+        <span className="glowing-txt">
+          S<span className="faulty-letter">K</span>ILLS
+        </span>
+      </button>
+      <p className="skills-text">Estudo linguagens como</p>
+    </section>
+  );
+};
 
 export default Skills;
--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import projects from "./works.js";
+import "./works.css";
 
 function Works(props) {
-    return (
-        <section id="works-props">
-            <h2 className="work-title">{ props.title }</h2>
-            <p className="work-content">{ props.content }</p>
-        </section>
-    );
+  return (
+    <section className="works-section">
+      <div className="works-items">
+        <a className="external" target="_blank" href="">
+          <img src="src/assets/icons/github.png" alt="icon-external" />
+        </a>
+        <a className="external" target="_blank" href="">
+          <img src="src/assets/icons/external.png" alt="icon-external" />
+        </a>
+        <h2 className="works-title">{props.title}</h2>
+        <p className="works-content">{props.content}</p>
+      </div>
+    </section>
+  );
 }
 
 export default Works;
