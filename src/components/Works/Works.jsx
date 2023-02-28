060eafa754bc451fdf9532c66c6d95198f52842d
dgmaikon <dgmaikondev@gmail.com>
2023-02-28 20:25:19 -0300

    Change img to fa-icon

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import projects from "./works.js";
 import "./works.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function Works(props) {
   return (
     <section className="works-section">
       <div className="works-items">
-        <a className="external" target="_blank" href="">
-          <img src="src/assets/icons/github.png" alt="icon-external" />
-        </a>
-        <a className="external" target="_blank" href="">
-          <img src="src/assets/icons/external.png" alt="icon-external" />
+        <a
+          className="external"
+          target="_blank"
+          href="https://github.com/dgmaikon?tab=repositories"
+        >
+          <FontAwesomeIcon icon="fa-brands fa-github" />
         </a>
         <h2 className="works-title">{props.title}</h2>
         <p className="works-content">{props.content}</p>
