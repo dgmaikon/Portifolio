a92a43940837dc14532f4f78283874e5d7c2fbd7
dgmaikon <dgmaikondev@gmail.com>
2023-03-02 16:45:41 -0300

    Add icon to external link, change section Works.

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -8,15 +8,20 @@ function Works(props) {
   return (
     <section className="works-section">
       <div className="works-items">
-        <a
-          className="external"
-          target="_blank"
-          href="https://github.com/dgmaikon?tab=repositories"
-        >
-          <FontAwesomeIcon icon="fa-brands fa-github" />
-        </a>
         <h2 className="works-title">{props.title}</h2>
         <p className="works-content">{props.content}</p>
+        <div className="works-links">
+          <a
+            className="icon-link"
+            target="_blank"
+            href="https://github.com/dgmaikon?tab=repositories"
+          >
+            <FontAwesomeIcon icon="fa-brands fa-github" />
+          </a>
+          <a className="icon-link" target="_blank" href="">
+            <FontAwesomeIcon icon="fa-solid fa-link" />
+          </a>
+        </div>
       </div>
     </section>
   );
