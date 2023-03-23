b79c501bc0cd7b585406ff77dcd4d6be300c7875
dgmaikon <dgmaikondev@gmail.com>
2023-03-23 19:49:14 -0300

    Add fontAwesome to file and render

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import "./styles.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHtml5,
+  faCss3Alt,
+  faJs,
+  faReact,
+  faNodeJs,
+  faGitAlt,
+  faGithub,
+  faSass,
+  faBootstrap,
+} from "@fortawesome/free-brands-svg-icons";
+
 const Skills = () => {
   return (
     <section id="skills">
@@ -9,7 +22,56 @@ const Skills = () => {
           S<span className="faulty-letter">K</span>ILLS
         </span>
       </button>
-      <p className="skills-text">Estudo linguagens como</p>
+
+      <div className="cards-wrapper">
+        <ul className="cards-list">
+          <li className="card-item">
+            <i className="card-link" href="https://www.instagram.com/dgmaikon/">
+              <FontAwesomeIcon icon={faHtml5} />
+            </i>
+          </li>
+          <li className="card-item">
+            <i className="card-link" href="https://www.instagram.com/dgmaikon/">
+              <FontAwesomeIcon icon={faCss3Alt} />
+            </i>
+          </li>
+          <li className="card-item">
+            <i className="card-link" href="https://www.instagram.com/dgmaikon/">
+              <FontAwesomeIcon icon={faJs} />
+            </i>
+          </li>
+          <li className="card-item">
+            <i className="card-link" href="https://www.instagram.com/dgmaikon/">
+              <FontAwesomeIcon icon={faReact} />
+            </i>
+          </li>
+          <li className="card-item">
+            <i className="card-link" href="https://www.instagram.com/dgmaikon/">
+              <FontAwesomeIcon icon={faNodeJs} />
+            </i>
+          </li>
+          <li className="card-item">
+            <i className="card-link" href="https://www.instagram.com/dgmaikon/">
+              <FontAwesomeIcon icon={faGitAlt} />
+            </i>
+          </li>
+          <li className="card-item">
+            <i className="card-link" href="https://www.instagram.com/dgmaikon/">
+              <FontAwesomeIcon icon={faGithub} />
+            </i>
+          </li>
+          <li className="card-item">
+            <i className="card-link" href="https://www.instagram.com/dgmaikon/">
+              <FontAwesomeIcon icon={faSass} />
+            </i>
+          </li>
+          <li className="card-item">
+            <i className="card-link" href="https://www.instagram.com/dgmaikon/">
+              <FontAwesomeIcon icon={faBootstrap} />
+            </i>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 };
