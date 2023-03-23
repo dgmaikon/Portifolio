0a677312a7a23d7772b1f074c6063044c733b7f3
dgmaikon <dgmaikondev@gmail.com>
2023-03-23 19:55:06 -0300

    Add about content and render

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,22 +1,69 @@
 import React from "react";
 import "./styles.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faDiscord,
+  faLinkedinIn,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
 const About = () => {
   return (
-    <div id="about">
+    <article id="about">
       <button className="glowing-btn">
         <span className="glowing-txt">
           A<span className="faulty-letter">B</span>OUT
         </span>
       </button>
-      <p className="about-text">
-        Olá me chamo Maikon Natã sou Front-End Developer,
-        <span>
-          {" "}
-          Estou sempre procurando aprender coisas novas e melhorando a cada dia.
-        </span>
-      </p>
-    </div>
+      <div className="about-wrapper">
+        <article className="about-profile"></article>
+        <div className="about-content">
+          <p className="about-text">
+            Meu nome é Maikon Natã, tenho 25 anos, sou Front-End developer. Eu
+            trabalho com HTML, CSS e Javascript para desenvolver interfaces
+            amigáveis e intuitivas para os usuários.
+          </p>
+          <div className="cards-wrapper">
+            <ul className="cards-list">
+              <li className="card-item">
+                <a
+                  className="card-link"
+                  href="https://www.instagram.com/dgmaikon/"
+                >
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+              </li>
+              <li className="card-item">
+                <a
+                  className="card-link"
+                  href="https://www.instagram.com/dgmaikon/"
+                >
+                  <FontAwesomeIcon icon={faDiscord} />
+                </a>
+              </li>
+              <li className="card-item">
+                <a
+                  className="card-link"
+                  href="https://www.instagram.com/dgmaikon/"
+                >
+                  <FontAwesomeIcon icon={faLinkedinIn} />
+                </a>
+              </li>
+              <li className="card-item">
+                <a
+                  className="card-link"
+                  href="https://www.instagram.com/dgmaikon/"
+                >
+                  <FontAwesomeIcon icon={faYoutube} />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </article>
   );
 };
 
