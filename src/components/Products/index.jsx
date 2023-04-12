b8a933835c8275438dbb9985a18b0817ca74b5e9
dgmaikon <dgmaikondev@gmail.com>
2023-04-12 13:37:29 -0300

    Create a folder for Work component

---
+++ src/components/Products/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "../Works";
+import Title from "../Works/Title-Work";
+
+const Products = () => {
+  return (
+    <>
+      <Title />
+      <Card />
+    </>
+  );
+};
+
+export default Products;
