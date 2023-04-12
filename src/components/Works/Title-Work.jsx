49b7e9496ffe62aa24d00fc17a681937c67a5d96
dgmaikon <dgmaikondev@gmail.com>
2023-04-12 13:44:39 -0300

    Add Motion Framer to file and render

--- src/components/Works/Title-Work.jsx
+++ src/components/Works/Title-Work.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+const TitleVariants = {
+  visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
+  hidden: { opacity: 0, y: 100 },
+};
 
 const Title = () => {
-    return <button className="glowing-btn"><span className='glowing-txt'>W<span className='faulty-letter'>O</span>RKS</span></button>
-}
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  return (
+    <motion.button
+      className="glowing-btn"
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      variants={TitleVariants}
+    >
+      <span className="glowing-txt">
+        W<span className="faulty-letter">O</span>RKS
+      </span>
+    </motion.button>
+  );
+};
 
 export default Title;
