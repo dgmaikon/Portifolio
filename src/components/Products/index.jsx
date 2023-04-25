ec8614802cdf6cc634a6cbd513d4e31e8d02f3a7
dgmaikon <dgmaikondev@gmail.com>
2023-04-24 21:21:09 -0300

    Ref: Add Framer motion to component

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,13 +1,34 @@
-import React from "react";
-import TitleWorks from "../Works/Title-Work";
-import Projects from "../Works";
+import React, { useEffect } from "react";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { fadeVariants } from "../../Variables";
+
+import TitleWorks from "../../objects/Works/Title-Work";
+import Projects from "../../objects/Works";
+
+import "./styles.css";
 
 const Products = () => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
   return (
-    <article className="products">
+    <motion.article
+      className="products"
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      variants={fadeVariants}
+    >
       <TitleWorks />
       <Projects />
-    </article>
+    </motion.article>
   );
 };
 
