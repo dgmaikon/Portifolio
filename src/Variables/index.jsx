368d75b17ec85bc5946cc6346889d6533c77fb5c
dgmaikon <dgmaikondev@gmail.com>
2023-04-26 13:49:19 -0300

    Ref: Change TitleVariant to file

--- src/Variables/index.jsx
+++ src/Variables/index.jsx
@@ -2,3 +2,8 @@ export const fadeVariants = {
   visible: { opacity: 1, y: 0, transition: { duration: 0.9 } },
   hidden: { opacity: 0, y: -40 },
 };
+
+export const TitleVariants = {
+  visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
+  hidden: { opacity: 0, y: 100 },
+};
