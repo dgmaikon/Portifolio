2b4454551f4ee0ac4892be17a7639ac6862dccc4
dgmaikon <dgmaikondev@gmail.com>
2023-04-28 07:08:45 -0300

    Create Folder for Navigation for mobile

---
+++ src/components/NavigationFooter/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import Logo from "../../objects/Logo";
+
+import "./styles.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBriefcase,
+  faCode,
+  faHouse,
+} from "@fortawesome/free-solid-svg-icons";
+import { faAddressCard } from "@fortawesome/free-regular-svg-icons";
+
+const NavigationFooter = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <nav className="nav-footer">
+      <ul className={width < 481 ? "visible" : "invible"}>
+        <li className="nav-items-footer">
+          <a className="nav-link" href="#main">
+            <FontAwesomeIcon icon={faHouse} />
+          </a>
+        </li>
+        <li className="nav-items-footer">
+          <a className="nav-link" href="#about">
+            <FontAwesomeIcon icon={faAddressCard} />
+          </a>
+        </li>
+        <li className="nav-items-footer">
+          <a className="nav-link">
+            <Logo />
+          </a>
+        </li>
+        <li className="nav-items-footer">
+          <a className="nav-link" href="#skills">
+            <FontAwesomeIcon icon={faCode} />
+          </a>
+        </li>
+        <li className="nav-items-footer">
+          <a className="nav-link" href="#project-section">
+            <FontAwesomeIcon icon={faBriefcase} />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavigationFooter;
