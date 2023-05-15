5ff8d9ea689a22cd0c8c5192264b1c69e0ad14b4
Maikon Natan <maikon@pop-os.localdomain>
2023-05-15 14:36:15 -0300

    Ref: Add useState and useEffect and Preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
 //Components JSX
-import Header from "./components/Header";
 import Title from "./components/Title";
 import About from "./components/About";
 import Skills from "./components/Skills";
@@ -10,23 +9,37 @@ import Products from "./components/Products";
 import Footer from "./components/Footer";
 import Button from "./components/Button";
 import NavigationFooter from "./components/NavigationFooter";
-import Content from "./components/LoaderContent";
+import PreLoader from "./components/PreLoader";
+import Navbar from "./components/Navbar";
 
 //CSS
 import "./App.css";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <>
-      <Content />
-      <Header />
-      <Title />
-      <About />
-      <Skills />
-      <Products />
-      <Button />
-      <Footer />
-      <NavigationFooter />
+      {isLoading ? (
+        <PreLoader />
+      ) : (
+        <>
+          <Navbar />
+          <Title />
+          <About />
+          <Skills />
+          <Products />
+          <Button />
+          <Footer />
+          <NavigationFooter />
+        </>
+      )}
     </>
   );
 };
