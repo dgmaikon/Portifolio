b21e5c5aeec979a33af1ca3483f926a3999aeaeb
Maikon Natan <maikon@pop-os.localdomain>
2023-05-18 14:07:22 -0300

    Ref: remove useEffect import.

--- src/objects/Works/index.jsx
+++ src/objects/Works/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
